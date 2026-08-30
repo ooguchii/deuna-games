59738ec9841fa95766d45a82850262219bc7fc55
ooguchii <fernandomembrilla69@gmail.com>
2026-08-30 11:27:54 -0300

    Remove duplicate genre section from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
 import FeaturedCategories from "@/components/home/FeaturedCategories";
-import FeaturedGenres from "@/components/home/FeaturedGenres";
 import GameFinderSection from "@/components/home/GameFinderSection";
 import GamesForYourPC from "@/components/home/GamesForYourPC";
 import HeroSection from "@/components/home/HeroSection";
@@ -110,9 +109,6 @@ export default async function Home() {
         <GameFinderSection />
         {games.length > 0 && (
           <FeaturedCategories games={games} />
-        )}
-        {games.length > 0 && (
-          <FeaturedGenres games={games} />
         )}
         {collections.recentGames.length > 0 && (
           <RecentlyAdded games={collections.recentGames} />
