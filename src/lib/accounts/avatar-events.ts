e0ca21d5b8fa2af34996b2a4271536c1625dee89
ooguchii <fernandomembrilla69@gmail.com>
2026-09-08 01:57:14 -0300

    Hacer persistente la señal cliente de avatar

--- src/lib/accounts/avatar-events.ts
+++ src/lib/accounts/avatar-events.ts
@@ -1,2 +1,34 @@
-export const ACCOUNT_AVATAR_CHANGED_EVENT =
-  "deuna:account-avatar-changed";
+"use client";
+
+import {
+  useSyncExternalStore,
+} from "react";
+
+let revision = 0;
+const listeners = new Set<() => void>();
+
+function subscribe(listener: () => void) {
+  listeners.add(listener);
+  return () => listeners.delete(listener);
+}
+
+function getRevision() {
+  return revision;
+}
+
+function getServerRevision() {
+  return 0;
+}
+
+export function notifyAccountAvatarChanged() {
+  revision += 1;
+  for (const listener of listeners) listener();
+}
+
+export function useAccountAvatarRevision() {
+  return useSyncExternalStore(
+    subscribe,
+    getRevision,
+    getServerRevision
+  );
+}
