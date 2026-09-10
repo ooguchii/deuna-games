91fe80775b8cedd12558ea3ed9cbee22b13b016b
ooguchii <fernandomembrilla69@gmail.com>
2026-09-10 12:01:03 -0300

    refactor: use modular game media assignments workspace

--- src/components/admin/GameMultimediaEditor.tsx
+++ src/components/admin/GameMultimediaEditor.tsx
@@ -1,8 +1,7 @@
 import GameGalleryMediaManager from "@/components/admin/GameGalleryMediaManager";
 import GameMediaAccessibilityEditor from "@/components/admin/GameMediaAccessibilityEditor";
+import GameMediaAssignmentsWorkspace from "@/components/admin/GameMediaAssignmentsWorkspace";
 import GameMultimediaUtilityRail from "@/components/admin/GameMultimediaUtilityRail";
-import GameMultimediaWorkspaceContextual from "@/components/admin/GameMultimediaWorkspaceContextual";
-import GameVideoLibraryEditor from "@/components/admin/GameVideoLibraryEditor";
 
 import refinementStyles from "./GameMultimediaLayoutRefinements.module.css";
 import shellStyles from "./GameMultimediaShell.module.css";
@@ -18,8 +17,6 @@ type GameMultimediaEditorProps = {
 export default function GameMultimediaEditor({
   slug,
   revision,
-  coverImage,
-  heroImage,
   screenshots = [],
 }: GameMultimediaEditorProps) {
   return (
@@ -28,19 +25,9 @@ export default function GameMultimediaEditor({
         <div
           className={`${shellStyles.legacyWorkspaceHost} ${refinementStyles.assignmentHost}`}
         >
-          <GameMultimediaWorkspaceContextual
+          <GameMediaAssignmentsWorkspace
             slug={slug}
             revision={revision}
-            screenshotCount={screenshots.length}
-            initialCoverImage={coverImage}
-            initialHeroImage={heroImage}
-            initialScreenshots={screenshots}
-            videoEditor={
-              <GameVideoLibraryEditor
-                slug={slug}
-                revision={revision}
-              />
-            }
           />
         </div>
 
